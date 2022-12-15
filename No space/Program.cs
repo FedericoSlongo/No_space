cde08e9f687926640bf2fe4f6bf807a6e7369cfa
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-12-15 12:57:59 +0100

    Add files via upload

---
+++ No space/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No_space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Dez.txt");
+
+            while (true)
+            {
+                sw.WriteLine("Dez Nuts!");
+            }
+        }
+    }
+}
