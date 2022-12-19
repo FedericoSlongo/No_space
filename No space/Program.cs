533db5341a9372070dfae91dbbc374b2482b848f
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-12-19 12:33:43 +0100

    Added multythreading

--- No space/Program.cs
+++ No space/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace No_space
 {
@@ -11,11 +11,41 @@ namespace No_space
     {
         static void Main(string[] args)
         {
+            Thread thr1 = new Thread(The_Writer);
+            Thread thr2 = new Thread(The_Writer_THE_RETURN);
+            Thread thr3 = new Thread(No);
+            thr1.Start();
+            thr2.Start();
+            thr3.Start();
+        }
+
+        public static void The_Writer()
+        {
             StreamWriter sw = new StreamWriter("Dez.txt");
 
             while (true)
             {
-                sw.WriteLine("Dez Nuts!");
+                sw.WriteLine("Dez ");
+            }
+        }
+
+        public static void The_Writer_THE_RETURN()
+        {
+            StreamWriter sw = new StreamWriter("Nuts.txt");
+
+            while (true)
+            {
+                sw.WriteLine("Nuts!");
+            }
+        }
+
+        public static void No()
+        {
+            StreamWriter sw = new StreamWriter("I_am_a_cat.txt");
+
+            while (true)
+            {
+                sw.WriteLine("Miao");
             }
         }
     }
